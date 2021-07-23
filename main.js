7f02d9b82c8a282012fd477c67b7f9e1e64e2e7f
SamuelJHall91 <71100441+SamuelJHall91@users.noreply.github.com>
2021-07-23 17:42:53 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+// Array of random celeb
+
+const celebrities = ['Tom Cruise', 'David Beckham', 'Jennifer Aniston', 'Halle Berry', 'Rihanna', 'Jim Carey', 'Ryan Gosling', 'Mila Kunis', 'Leonardo DiCaprio', 'Cristiano Ronaldo', 'Scarlett Johansson', 'Dwayne Johnson'];
+
+// Array of initial start to statements
+
+const startList = ['decided to',
+               'thought it would be a good idea to',
+               'happened to',
+               'seemed to think it would be cool to'];
+
+// Array of random everyday activities
+
+const everydayList = ['hoover their house',
+                  'walk their dog',
+                  'clean their car',
+                  'cook some dinner',
+                  'take a shower',
+                  'brush their teeth',
+                  'take the bins out'];
+
+// Array of ridiculous actions
+
+const ridiculousList = ['whilst playing the flute',
+                    'whilst wearing a nappy on their head',
+                    'whilst drizzing chocolate sauce over themself',
+                    'whilst trying to balance a melon on their head',
+                    'whilst wearing a morph suit',
+                    'whilst blowing up balloons',
+                    'whilst juggling some oranges'];
+
+// start of the overall function to generate the random text
+
+
+
